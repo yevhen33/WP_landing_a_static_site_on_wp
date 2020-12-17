53b5ceb9c6dc7509810195523268d83600303375
Yevhen <yevhen.code@gmail.com>
2020-12-17 19:41:00 +0200

    разбили сайт на многостраничный

---
+++ page-team.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Template Name: Наша команда
+*/
+?>
+
+<?php 
+ get_header();
+?>
+
+<div class="specialists">
+    <div class="container">
+        <div class="title"><?php the_field('command_title'); ?></div>
+        <div class="row">
+            <div class="col-lg-10 offset-lg-1">
+                <?php 
+                    $image = get_field('command_img');
+                    if (!empty($image)): ?>
+
+                    <img class="specialists__img"
+                    src="<?php echo $image['url']; ?>"
+                    alt="<?php echo $image['alt']; ?>">
+
+                <?php endif;
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php 
+ get_footer();
+?>
---
+++ page-toys.php
@@ -0,0 +1,106 @@
+<?php
+/*
+Template Name: Игрушки
+*/
+?>
+
+<?php
+    get_header();
+?>
+<div class="toys">
+    <div class="container">
+        <h2 class="subtitle">Мягкие игрушки</h2>
+        <div class="toys__wrapper">
+
+        <?php 
+            // параметры по умолчанию
+            $posts = get_posts( array(
+                'numberposts' => -1,
+                'category_name'    => 'soft_toys',
+                'orderby'     => 'date',
+                'order'       => 'ASC',
+                'post_type'   => 'post',
+                'suppress_filters' => true, // подавление работы фильтров изменения SQL запроса
+            ) );
+
+            foreach( $posts as $post ){
+                setup_postdata($post);
+                ?>
+
+                <div class="toys__item" 
+                    style="background-image: url(<?php   // проверяем установлина ли превьюшка в 
+                        if (has_post_thumbnail()) {      // админке, если да - выводим ее
+                            the_post_thumbnail_url();    
+                        } else {                         // если нет выводим подготовленое фото
+                            echo get_template_directory_uri() . '/assets/img/not-found.jpg';
+                        }
+                    ?>)">
+                    <div class="toys__item-info">
+                        <div class="toys__item-title"><?php the_title() ?></div>
+                        <div class="toys__item-descr">
+                            <?php the_field('toys_descr'); ?>                            
+                        </div>
+                        <div class="minibutton toys__trigger">Подробнее</div>
+                    </div>
+                </div>
+
+            <?php
+            }
+
+            wp_reset_postdata(); // сброс
+        ?>
+        </div>
+
+
+        <h2 class="subtitle">Развивающие игрушки</h2>
+        <div class="toys__wrapper">
+
+        <?php 
+            
+            $posts = get_posts( array(
+                'numberposts' => -1,
+                'category_name'    => 'educ_toys',
+                'orderby'     => 'date',
+                'order'       => 'ASC',
+                'post_type'   => 'post',
+                'suppress_filters' => true, 
+            ) );
+
+            foreach( $posts as $post ){
+                setup_postdata($post);
+                ?>
+                <div class="toys__item" 
+                    style="background-image: url(<?php
+                        if (has_post_thumbnail()) {
+                            the_post_thumbnail_url();
+                        } else {
+                            echo get_template_directory_uri() . '/assets/img/not-found.jpg';
+                        }
+                    ?>)">
+                    <div class="toys__item-info">
+                        <div class="toys__item-title"><?php the_title(); ?></div>
+                        <div class="toys__item-descr">
+                            <?php the_field('toys_descr'); ?>    
+                        </div>
+                        <div class="minibutton toys__trigger">Подробнее</div>
+                    </div>
+                </div>
+            <?php
+            }
+
+            wp_reset_postdata(); 
+        ?>
+
+        </div>
+        <div class="row">
+            <div class="col-lg-10 offset-lg-1">
+                <div class="toys__alert">
+                    <span><?php the_field('question'); ?></span><?php the_field('answer'); ?> 
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+    get_footer();
+?>
