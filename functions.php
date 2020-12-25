d7b9db7eecaf69e1264ddedf0d7c9dafe8424809
Yevhen <yevhen.code@gmail.com>
2020-12-25 18:31:24 +0200

    добавили динамическое меню

--- functions.php
+++ functions.php
@@ -27,4 +27,19 @@
     }
     
     add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
+
+    add_theme_support( 'menus' ); // активируем меню
+
+    add_filter('nav_menu_link_attributes', 'filter_nav_menu_link_attributes', 10, 3);
+    function filter_nav_menu_link_attributes($atts, $item, $args) {
+        if ($args->menu === 'Main') {
+            $atts['class'] = 'header__nav-item';
+
+            if ($item->current) {
+                $atts['class'] .= ' header__nav-item-active';
+            }
+        };
+
+        return $atts;
+    }
 ?>
