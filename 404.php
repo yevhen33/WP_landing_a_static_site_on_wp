3a242ecbe26c0a97dbd3f53d0682eeac7dff3952
Yevhen <yevhen.code@gmail.com>
2021-01-21 12:01:44 +0200

    добавлена страница 404

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php
+    get_header();
+?>
+
+<div class="container toys">
+    <h2 class="subtitle">Что-то пошло нет так <br> страница не найдена! <br><br> <a href="<?php echo get_home_url(); ?>">Вернуться на главную?</a> </h2>
+
+</div>  
+
+<?php
+    get_footer();
+?>
